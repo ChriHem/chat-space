7de2813ebf783b6f4b6bd6a08041038d192f0211
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-06-26 05:21:29 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ set :application, 'chat-space'
 set :repo_url,  'git@github.com:ChriHem/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
@@ -15,8 +16,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
